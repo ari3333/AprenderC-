46a0260a65f8bf87d686c1370a89231a0f55f831
ari3333 <guardandoLMPrimero2@gmail.com>
2020-06-23 20:27:38 +0200

    Arrays, creacion y manipulacion

---
+++ Arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+
+            //declaraciones de arrays
+            //int[] numeros = new int[5];
+
+            //otra declaracion si sabemos que datos va tener mi array
+            int[] numeros = { 3, 13, 33, 83, 123};
+
+            //cambiar cualquiera de esos datos
+            numeros[3] = 35;
+
+
+            //acceder a posiciones del array
+            /* numeros[0] = 3;
+             numeros[1] = 13;
+             numeros[2] = 33;
+             numeros[3] = 83;
+             numeros[4] = 123;*/
+
+            /*si lo que queremos hacer es agrandar el array pues lo que podemos hacer es 
+            hacer otro array mas grande y añadirle los datos del anterior*/
+            int[] numeros2 = new int[10];
+
+            //recorrer un array
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                //copio los datos del array de arriba en el nuevo
+                numeros2[i] = numeros[i];
+                Console.WriteLine("El elemento " + (i + 1) + " es igual a " + numeros2[i]);
+            }
+
+            Console.WriteLine("****************************");
+
+            //recorrer un array
+            for (int i = 0; i < numeros2.Length; i++)
+            {
+                Console.WriteLine("El elemento " + (i + 1) + " es igual a " + numeros2[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Ciclos/Program.cs
+++ Ciclos/Program.cs
@@ -15,7 +15,7 @@ namespace Ciclos
 
             //incrementos ++ y decrementos --
 
-            int num = 0;
+            //int num = 0;
             /* while(num < 10)
              {
                  Console.WriteLine("Hola mundo");
@@ -24,17 +24,40 @@ namespace Ciclos
              */
 
             //do while  -has mientras
-            do
+            /*do
             {
                 Console.WriteLine("Hola mundo con do while \n");
                 num++;
 
-            } while (num < 10);
+            } while (num < 10);*/
+
+            //ciclo for - para
+            /*for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hola mundo con for \n");
+            }
+           */
+
+            dynamic valor, suma=0, promedio;
+            //ejercicio
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingresa el numero "+ i +" por favor.\n");
+                valor = Convert.ToDouble(Console.ReadLine());
+                suma = suma + valor;
+                      
+            }
+
+            promedio = suma / 5;
+
+            Console.WriteLine("El resultado de la suma es: " + suma);
+            Console.WriteLine("El resultado del promedio es: " + promedio);
+
+
             Console.ReadKey();
 
 
-
-            
         }
     }
 }
